951c09fbadcc698f3d5d2608fd6932b997479f3c
Michael Kim <mkim0407@gmail.com>
2019-04-05 12:16:44 -0700

    use with clause in std tests

--- tests/io/test_std.py
+++ tests/io/test_std.py
@@ -1,20 +1,32 @@
 from io import StringIO
 
+import pytest
+
 from stream.io.std import StdIn
 
 
+@pytest.mark.dependency(
+    depends=[
+        ('session', 'tests/io/test_base.py::test_with'),
+    ],
+)
 def test_stdin(monkeypatch):
     monkeypatch.setattr('sys.stdin', StringIO('abc\n123\n'))
 
-    f = StdIn()
-    assert tuple(f) == ('abc\n', '123\n')
+    with StdIn() as f:
+        assert tuple(f) == ('abc\n', '123\n')
 
 
+@pytest.mark.dependency(
+    depends=[
+        ('session', 'tests/io/test_base.py::test_with'),
+    ],
+)
 def test_stdin_buffer(tmpdir, monkeypatch):
     file = tmpdir / '0.txt'
     file.write_text('abc\n123\n', encoding='utf-8')
 
     monkeypatch.setattr('sys.stdin', open(str(file)))
 
-    f = StdIn()
-    assert tuple(f.buffer) == (b'abc\n', b'123\n')
+    with StdIn() as f:
+        assert tuple(f.buffer) == (b'abc\n', b'123\n')
